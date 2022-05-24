4f555a5189e0ee3136be2d9c84336d3953893304
Wolf McNally <wolf@wolfmcnally.com>
2022-05-23 17:48:16 -0700

    Added ImageExtensions.

---
+++ Sources/WolfBase/ImageExtensions.swift
@@ -0,0 +1,33 @@
+#if canImport(UIKit)
+import UIKit
+
+extension UIImage {
+    public func resized(to targetSize: CGSize, interpolationQuality: CGInterpolationQuality = .none) -> UIImage {
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        var newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        let context = UIGraphicsGetCurrentContext()!
+        context.interpolationQuality = interpolationQuality
+        draw(in: rect)
+        return UIGraphicsGetImageFromCurrentImageContext()!
+    }
+    
+    public func scaled(by scale: CGFloat, interpolationQuality: CGInterpolationQuality = .none) -> UIImage {
+        let scaledSize = CGSize(width: size.width * scale, height: size.height * scale)
+        return resized(to: scaledSize, interpolationQuality: interpolationQuality)
+    }
+}
+#endif
