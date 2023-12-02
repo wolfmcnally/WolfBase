91c8cad45e6325ab76db1b5b60c5f6616d52a0cb
Wolf McNally <wolf@wolfmcnally.com>
2023-12-01 17:13:50 -0800

    Update dependencies.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["WolfBase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
     ],
     targets: [
         .target(
