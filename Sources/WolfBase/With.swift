64953cfea10c57d01077ae77e76b8a72b472cb9c
Wolf McNally <wolf@wolfmcnally.com>
2024-03-12 20:36:39 -0700

    Improved `with` functions.

--- Sources/WolfBase/With.swift
+++ Sources/WolfBase/With.swift
@@ -1,113 +1,20 @@
 import Foundation
 
-/// The `with` function is based on [the Python `with` statement](https://peps.python.org/pep-0343/#examples).
-/// It comes in two forms, one that takes a value and returns a result,
-/// and one that takes an `inout` reference to an instance of a value type
-/// and performs some manipulation on it.
-///
-/// - Example:
-///
-/// ```
-/// func testWith() {
-///     struct Dog {
-///         var name: String
-///     }
-///
-///     var dog = Dog(name: "Puppy")
-///
-///     with(&dog) { d in
-///         d.name = "Magic"
-///     }
-///
-///     print(dog.name) // "Magic"
-///
-///     class Cat {
-///         var name: String
-///         init(name: String) { self.name = name }
-///         deinit { print("deinit \(name)") }
-///     }
-///
-///     let cat = with(Cat(name: "Kitten")) { c in
-///         c.name = "Elliot"
-///         return c
-///     }
-///
-///     with(cat) { c in
-///         print(c.name) // "Elliot"
-///     }
-///
-///     with(Cat(name: "Jellicle")) { c in
-///         print(c.name) // Jellicle
-///     }
-///
-///     print("after deinit Jellicle")
-/// }
-///
-/// // Prints:
-/// //   Magic
-/// //   Elliot
-/// //   Jellicle
-/// //   deinit Jellicle
-/// //   after deinit Jellicle
-/// //   deinit Elliot
-/// ```
-///
-public func with<T, Result>(_ t: T, op: (T) throws -> Result) rethrows -> Result {
+// Version 1: Transform an instance of an input type into an instance of
+// a possibly different output type.
+public func with<I, O>(_ t: I, op: (I) throws -> O) rethrows -> O {
     try op(t)
 }
 
-/// The `with` function is based on [the Python `with` statement](https://peps.python.org/pep-0343/#examples).
-/// It comes in two forms, one that takes a value and returns a result,
-/// and one that takes an `inout` reference to an instance of a value type
-/// and performs some manipulation on it.
-///
-/// - Example:
-///
-/// ```
-/// func testWith() {
-///     struct Dog {
-///         var name: String
-///     }
-///
-///     var dog = Dog(name: "Puppy")
-///
-///     with(&dog) { d in
-///         d.name = "Magic"
-///     }
-///
-///     print(dog.name) // "Magic"
-///
-///     class Cat {
-///         var name: String
-///         init(name: String) { self.name = name }
-///         deinit { print("deinit \(name)") }
-///     }
-///
-///     let cat = with(Cat(name: "Kitten")) { c in
-///         c.name = "Elliot"
-///         return c
-///     }
-///
-///     with(cat) { c in
-///         print(c.name) // "Elliot"
-///     }
-///
-///     with(Cat(name: "Jellicle")) { c in
-///         print(c.name) // Jellicle
-///     }
-///
-///     print("after deinit Jellicle")
-/// }
-///
-/// // Prints:
-/// //   Magic
-/// //   Elliot
-/// //   Jellicle
-/// //   deinit Jellicle
-/// //   after deinit Jellicle
-/// //   deinit Elliot
-/// ```
-///
+// Version 2: Perform an in-place mutation of an instance.
 public func with<T>(_ t: inout T, op: (inout T) throws -> Void) rethrows {
     try op(&t)
+}
+
+// Version 3: Perform a mutation on a copy of an instance and return the
+// modified copy.
+public func with<T>(_ t: T, op: (inout T) throws -> Void) rethrows -> T {
+    var t = t
+    try op(&t)
+    return t
 }
