423eef4d0a11b550aae2ad949f435684d7046834
Wolf McNally <wolf@wolfmcnally.com>
2024-04-11 01:03:54 -0700

    Refined `with` functions.

--- Sources/WolfBase/With.swift
+++ Sources/WolfBase/With.swift
@@ -1,18 +1,12 @@
 import Foundation
 
-// Version 1: Transform an instance of an input type into an instance of
-// a possibly different output type.
-public func with<I, O>(_ t: I, op: (I) throws -> O) rethrows -> O {
-    try op(t)
-}
-
-// Version 2: Perform an in-place mutation of an instance.
+// Perform an in-place mutation of an instance.
 public func with<T>(_ t: inout T, op: (inout T) throws -> Void) rethrows {
     try op(&t)
 }
 
-// Version 3: Perform a mutation on a copy of an instance and return the
-// modified copy.
+// Perform a mutation on a copy of an instance and return the modified copy.
+@discardableResult
 public func with<T>(_ t: T, op: (inout T) throws -> Void) rethrows -> T {
     var t = t
     try op(&t)
